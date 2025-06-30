9fbf3b202112d5e8bc6b17d205c800c062b5348d
Dilshan Herath <137473394+MadusankhaHerath@users.noreply.github.com>
2025-06-30 23:16:53 +0530

    chainnode

--- Chainnode.java
+++ Chainnode.java
@@ -3,12 +3,14 @@ class Chainnode{
     Object data;
 
 
-chainnode(Object d){
+Chainnode(Object d){
     this.data = d;
     this.next = null;
     }
-}
+
 Chainnode(Object d, Chainnode n){
     this.data = d;
     this.next = n;
+    }
+
 }
