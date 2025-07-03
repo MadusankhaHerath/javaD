5abaa7e4d40002097314ff426959881e542c89b8
Dilshan Herath <137473394+MadusankhaHerath@users.noreply.github.com>
2025-07-03 16:16:20 +0530

    arithmatic exception

---
+++ exception1.java
@@ -0,0 +1,13 @@
+public class exception1{
+    public static void main(String[] args){
+        int a=10;
+        int b=5;
+        int c=5;
+
+        int num = a/(b-c);
+        //arithmatic exceptiion because number devide by zero
+        System.out.println("num : " +num);
+
+
+    }
+}
