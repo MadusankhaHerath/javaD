b28e2745793f158dbf7639a1479c2d115b35f5f0
Dilshan Herath <137473394+MadusankhaHerath@users.noreply.github.com>
2025-07-05 23:07:13 +0530

    number format exception

--- exception2.java
+++ exception2.java
@@ -1,9 +1,16 @@
 public class exception2{
     public static void main(String [] arg){
+         System.out.println("hello" );
+        try{
         String num = "123a";
         //number format exception
+        
         int value = Integer.parseInt(num);
+        }
+        catch(NumberFormatException e){
+            System.out.println(e);
+        }
 
-        System.out.println("value :" +value);
+       
     }
 }
